119eb74d65a070230e60043703397bdf52665759
T-H-Jun <tanghuajun20020821@163.com>
2020-12-27 22:20:21 +0800

    add a new code

---
+++ sushu.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for (i=2;i<=a;i++)
+    {
+	if(a%i==0&&i!=a)
+	{
+	    printf("no");
+	    break;
+	}
+	else if(i==a)
+	{
+	    printf("yes");
+	}
+    }
+    return 0;
+}
